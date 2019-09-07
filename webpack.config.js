6f6662828ec1f746eff649d4288ce354f47eea7f
nodejs <1789500304@qq.com>
2019-09-07 11:51:59 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 
 var addUI = require('./addUI');
-
+var entryFile = fs.readFileSync(
+  path.join(__dirname, 'packFilePath.js'),
+  'utf8'
+);
+console.log('webpack entryFile = ' + entryFile);
 module.exports = {
-  entry:  __dirname + "/app/index.js",//已多次提及的唯一入口文件
+  entry: entryFile, //已多次提及的唯一入口文件
   output: {
-    path: __dirname + "/public",//打包后的文件存放的地方
-    filename: "bundle.js"//打包后输出文件的文件名
+    path: __dirname + '/public', //打包后的文件存放的地方
+    filename: 'bundle.js' //打包后输出文件的文件名
   },
-  plugins: [
-    new addUI({options: true})
-  ]
-}
+  plugins: [new addUI({ options: true })],
+  mode: 'production',
+  module: {
+    rules: [
+      //添加 xml-loader
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      }
+    ]
+  }
+};
