7aefe9676422d236c5ddee738982905af1255773
nodejs <1789500304@qq.com>
2019-09-07 11:52:41 +0800

    Update addUI.js

--- addUI.js
+++ addUI.js
@@ -1,15 +1,27 @@
 const fs = require('fs');
+const path = require('path');
 
+var entryFile = fs.readFileSync(
+  path.join(__dirname, 'packFilePath.js'),
+  'utf8'
+);
+
+
+console.log(entryFile);
 function addUI(options) {}
-addUI.prototype.apply = function (compiler) {
-  compiler.plugin('emit', function (compilation, callback) {
-    fs.readFile(__dirname + "/app/index.js", 'utf8', (err, data) => {
+addUI.prototype.apply = function(compiler) {
+  compiler.plugin('emit', function(compilation, callback) {
+    fs.readFile(entryFile, 'utf8', (err, data) => {
       if (err) throw err;
       if (/['"]ui['"]/.test(data)) {
-        console.log('包含"ui"')
-        compilation.assets['bundle.js']._value = '"ui";' + compilation.assets['bundle.js']._value
+        console.log('包含"ui"');
+        var layoutContentFileParentDirectory = path.join(path.dirname(entryFile), 'layout.xml')
+        var layoutContent = fs.readFileSync(layoutContentFileParentDirectory);
+        var UI = `ui.layout(${layoutContent});`;
+        compilation.assets['bundle.js']._value =
+          '"ui";' + UI + compilation.assets['bundle.js']._value;
       } else {
-        console.log('不包含"ui"')
+        console.log('不包含"ui"');
       }
       callback();
     });
