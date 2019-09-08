c2f658f6774602f3b9a98c956891019eb46e1fab
autojs <203118908@qq.com>
2019-09-09 01:14:31 +0800

    '配置文件增加base64选项'

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 const path = require("path");
-console.log('__dirname: '+__dirname)
 var config = {
   entry: path.resolve('.', "app", "index.js"),
   base64: false,
-  log: true
+  log: false
 };
 
 module.exports = config;
